b72e805ec49dc81202249e800bbd32d6d0f9ea0d
Ponleou Keo Sok <ponleousk@gmail.com>
2023-09-19 18:13:55 +0700

    feat: add coordinates and names of selected stops

---
+++ src/supportedStopsDT.js
@@ -0,0 +1,98 @@
+exports.DTStops = [
+  {
+    name: "DT3",
+    coords: {
+      lat: 1.3622305689825998,
+      lng: 103.76720307786846,
+    },
+    timeArrival: null,
+  },
+  {
+    name: "DT5",
+    coords: {
+      lat: 1.3426916956667438,
+      lng: 103.77599425717294,
+    },
+    timeArrival: null,
+  },
+  {
+    name: "DT8",
+    coords: {
+      lat: 1.3263399761632182,
+      lng: 103.80796589552126,
+    },
+    timeArrival: null,
+  },
+  {
+    name: "DT11",
+    coords: {
+      lat: 1.31469709879026,
+      lng: 103.8382037743078,
+    },
+    timeArrival: null,
+  },
+  {
+    name: "DT13",
+    coords: {
+      lat: 1.3044308245141776,
+      lng: 103.85255584333447,
+    },
+    timeArrival: null,
+  },
+  {
+    name: "DT16",
+    coords: {
+      lat: 1.2819370850743004,
+      lng: 103.8589666347505,
+    },
+    timeArrival: null,
+  },
+  {
+    name: "DT19",
+    coords: {
+      lat: 1.2851170327988097,
+      lng: 103.8434244885373,
+    },
+    timeArrival: null,
+  },
+  {
+    name: "DT21",
+    coords: {
+      lat: 1.2990946501303768,
+      lng: 103.84997176913734,
+    },
+    timeArrival: null,
+  },
+  {
+    name: "DT23",
+    coords: {
+      lat: 1.3143548847291047,
+      lng: 103.86338897488652,
+    },
+    timeArrival: null,
+  },
+  {
+    name: "DT26",
+    coords: {
+      lat: 1.327675688322908,
+      lng: 103.89009254440981,
+    },
+    timeArrival: null,
+  },
+  {
+    name: "DT29",
+    coords: {
+      lat: 1.3360738625353232,
+      lng: 103.91828464073042
+    },
+    timeArrival: null,
+  },
+  {
+    name: "DT31",
+    coords: {
+      lat: 1.3467309790778712,
+      lng: 103.93775102865686
+    },
+    timeArrival: null,
+  },
+];
