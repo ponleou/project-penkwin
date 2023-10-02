1284034e20b974ec72f1b2a9c9f000bd2bdd9592
Edward <edwardchhun3@gmail.com>
2023-10-02 03:04:54 -0700

    Update

--- societal_status.py
+++ societal_status.py
@@ -9,62 +9,111 @@ next_month_date = today_date + one_month_duration
 
 # User Variables
 uber_credits = 0
-
+user_ID = []
+# Variables that are going to be sent to json files that are needed to be used
+# dummy_variables = [student_id, poverty, uber_credits, help, user_input]
 # Status checker for user
 
-working_student = 0
+working = 0
+student = 0
 poverty = 0
 accommodation = 0
 
-# Check for working/student status
+# Check user ID 
 
-def check_status_working_student():
-    global working_student
+def check_user_id():
+    global user_id
+    print("Check User")
 
-    if working_student == 0:
-        verification = 0
-        student_id = input("\nPlease enter your student ID number: ")
-        print("\nYour student ID is:", student_id)
+# Check for working status
 
-        # Needs error handling for student verification
-        print("\nWe are currenly checking your student status, please give one business day for a response.")
-        verification += 1
+def check_status_working():
+    global working
+    global working_role
+    global working_organization
+    eligibility = input("\nAre you eligibile for organization employee benefits? (Y/N): ")
+    if eligibility == "Y":
+        if working == 0:
+            working_organization = input("Please enter your organization's name: ")
+            print("\n", working_organization)
+            working_role = input("\nPlease enter your role: ")
+            print("\n", working_role)
+            print("\nThank you for your information, we are currently checking your status", 
+                  " please give one business day for a response.")
+            working += 1
+            print("\nWe have updated your role, your ride information is going to be shared to help",
+                  "cater transportation times to improve productivity accross Uberland.")
+        # Needs to error handle (Assign data to database to make sure user is legit)
+    elif eligibility == "N":
+        print("\nThank you for answering!")
+    else:
+        print("Please type in the correct input, example: Y / N")
+        check_status_working()
 
-        if verification == 1:
-            working_student += 1
-            return working_student
-        else:
-            print("\nUnfortunately, we weren't able to process your applications\n"
-                       "Please contact customer support: (012-223-223)")
+# Check for student status
+
+def check_status_student():
+    global student
+    global student_id
+    eligibility = input("\nDo you want to sign up for student's discount? (Y/N): ")
+    if eligibility == "Y":
+        if student == 0:
+            verification = 0
+            student_id = input("\nPlease enter your student ID number: ")
+            print("\nYour student ID is:", student_id)
+
+            # Needs error handling for student verification
+            print("\nWe are currenly checking your student status, please give one business day for a response.")
+            verification += 1
+
+            if verification == 1:
+                student += 1
+                return student
+            else:
+                print("\nUnfortunately, we weren't able to process your applications\n"
+                        "Please contact customer support: (012-223-223)")
+    if eligibility == "N":
+        print("\nThank you for answering!")
+    else:
+        print("Please type in the correct input, example: Y / N")
+        check_status_student()
 
 
-
+# NEEDS ERROR HANDLING
 def check_status_poverty():
     global poverty
     global uber_credits
+    global annual_income
+    eligibility = input("\nDo you want to check if you are eligible for financial aid in Uber Credits? (Y/N): ")
+    if eligibility == "Y":
+        if poverty == 0:
+                annual_income = int(input("\nPlease enter your estimate annual income: $ "))
+                print("\nYour annual income is: $",annual_income)
+                print("\nThe system is processing numbers.....")
+                if annual_income <= 24000:
+                    print("\nYou are eligible to $70.00 of Uber Credits per month starting from", dt.datetime.now(),"\n"
+                        "until", next_month_date, "\n")
+                    uber_credits += 70.00
+                    poverty += 1
+                    
+                else:
+                    print("\nUnfortunately, we weren't able to process your applications\n"
+                            "Please contact customer support: (012-223-223)")
+    elif eligibility == "N":
+        print("\nThank you for answering!")
+    else:
+        print("Please type in the cor`rect input, example: Y / N")
+        check_status_poverty()
 
-    if poverty == 0:
-        annual_income = int(input("\nPlease enter your estimate annual income: $ "))
-        print("\nYour annual income is: $",annual_income)
-        print("\nThe system is processing numbers.....")
-        if annual_income <= 24000:
-            print("\nYou are eligible to $70.00 of Uber Credits per month starting from", dt.datetime.now(),"\n"
-                  "until", next_month_date, "\n")
-            uber_credits += 70.00
-            poverty += 1
-            
-        else:
-            print("\nUnfortunately, we weren't able to process your applications\n"
-                       "Please contact customer support: (012-223-223)")
-            
 def check_status_accommodation():
     global accommodation
+    global help
 
     if accommodation == 0:
 
         # Needs fixed input of 1 character
 
-        help = input("Do you require accommodation when riding with Uber (Y/N): ")
+        help = input("\nDo you require accommodation when riding with Uber (Y/N): ")
 
         if help == "Y":
             accommodation += 1
@@ -81,9 +130,9 @@ def check_status_accommodation():
 # Predefined transportation schedule
 
 transportation_schedule = {
-    "Morning": datetime.time(8, 0),
-    "Afternoon": datetime.time(14, 0),
-    "Evening": datetime.time(18, 0),
+    "Morning": dt.time(8,0),
+    "Afternoon": dt.time(12,0),
+    "Evening": dt.time(17,0),
 }
 
 # Function to check if the user's requested time is available
@@ -95,11 +144,13 @@ def check_time_availability(user_time, schedule):
 
 # Prompt the user for their time availability
 while True:
-    user_input = input("Please enter your preferred time (e.g., '08:00 AM'): ")
+    global user_preferred_time
+    print("\nThe time available for public transportation are from\n08:00 AM - 10:00 PM")
+    user_preferred_time = input("\nPlease enter your preferred time (e.g., '08:00 AM'): ")
     
     # Parse user input to a datetime.time object
     try:
-        user_time = datetime.datetime.strptime(user_input, '%I:%M %p').time()
+        user_time = dt.datetime.strptime(user_preferred_time, '%I:%M %p').time()
     except ValueError:
         print("Invalid time format. Please use 'hh:mm AM/PM' format.")
         continue
@@ -117,10 +168,11 @@ while True:
 
 # Calling the functions
 
-check_status_working_student()
+check_status_working()
+check_status_student()
 check_status_poverty()
 check_status_accommodation()
-print("\nWorking Student: ", working_student, "\nFinancial Aid: ", poverty , "\nRequires Accommodation", accommodation)
+print("\nWorking Student: ", student, "\nFinancial Aid: ", poverty , "\nRequires Accommodation", accommodation)
 
 #--------------------------------
 
@@ -132,9 +184,15 @@ users['info'] = []
 
 # Create a dictionary with user information
 user_info = {
-    "working_student": working_student,
-    "poverty": poverty,
-    "accommodation": accommodation,
+    # 0 is False, 1 is True
+    "User Preferred Time": user_preferred_time,
+    "Student ID" : student_id,
+    "Working": working,
+    "Job Role": str(working_role),
+    "Job Organization": str(working_organization),
+    "Student": student,
+    "Financial Aid": poverty,
+    "Accommodation": accommodation,
 }
 
 file_name = "user_info.json"
